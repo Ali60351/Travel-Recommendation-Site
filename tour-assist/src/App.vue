c33384cdd4b6c4c71a619ec6ca4746304f302654
Ali Nauman <ali60351@gmail.com>
2018-03-05 04:56:21 +0500

    Implemented basic functionality

--- tour-assist/src/App.vue
+++ tour-assist/src/App.vue
@@ -12,14 +12,16 @@
       <v-list>
         <v-list-tile
           value="true"
-          v-for="(item, i) in items"
-          :key="i"
+          v-for="item in items"
+          :key="item.title"
+          @click="item.onClick"
+          v-bind:disabled="item.disabled"
         >
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon dark v-html="item.icon"></v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title class="white--text" v-text="item.title"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -37,8 +39,13 @@
         <v-icon>web</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn @click="loginDialog = true" flat><v-icon left dark>input</v-icon>Login</v-btn>
+        <v-btn @click="signUpDialog = true" flat><v-icon left dark>launch</v-icon>Sign Up</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-    <v-content> <!-- Insert Main Content here -->
+    <v-content v-if="isHome"> <!-- Insert Main Content here -->
       <section>
         <v-parallax :src="require('@/assets/1.jpg')" height="400">
           <v-layout
@@ -54,7 +61,7 @@
               class="blue lighten-2 mt-5"
               dark
               large
-              href="/pre-made-themes"
+              v-on:click="test"
             >
               Get Started
             </v-btn>
@@ -200,6 +207,72 @@
         </v-container>
       </section>
     </v-content>
+    <v-dialog v-model="loginDialog" max-width="290">
+      <v-card>
+        <v-card-title class="headline">Use Google's location service?</v-card-title>
+        <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" flat="flat" @click.native="loginDialog = false">Disagree</v-btn>
+          <v-btn color="green darken-1" flat="flat" @click.native="loginDialog = false">Agree</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="signUpDialog" max-width="500">
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal first name" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  label="Legal last name"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Email" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Password" type="password" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select
+                  label="Age"
+                  required
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                ></v-select>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select
+                  label="Interests"
+                  multiple
+                  autocomplete
+                  chips
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                ></v-select>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="signUpDialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="signUpDialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-footer :fixed="fixed" app>
       <span>&copy; Tour Assist 2017</span>
     </v-footer>
@@ -211,20 +284,124 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
+      clipped: true,
+      drawer: true,
       fixed: false,
       items: [
         {
-          icon: 'bubble_chart',
-          title: 'Inspire'
+          icon: 'home',
+          title: 'Home',
+          onClick: this.onClickHome,
+          disabled: true
+        },
+        {
+          icon: 'favorite',
+          title: 'Attractions',
+          onClick: this.onClickAttractions,
+          disabled: false
+        },
+        {
+          icon: 'restaurant',
+          title: 'Resturants',
+          onClick: this.onClickResturants,
+          disabled: false
+        },
+        {
+          icon: 'local_hotel',
+          title: 'Accomodation',
+          onClick: this.onClickAccomodation,
+          disabled: false
+        },
+        {
+          icon: 'flight_takeoff',
+          title: 'Travel',
+          onClick: this.onClickTravel,
+          disabled: false
         }
       ],
-      miniVariant: false,
+      miniVariant: true,
       right: true,
       rightDrawer: false,
-      title: 'Tour Assist'
+      title: 'Tour Assist',
+      isHome: true,
+      isAttractions: false,
+      isResturants: false,
+      isAccomodation: false,
+      isTravel: false,
+      loginDialog: false,
+      signUpDialog: false
     };
+  },
+  methods: {
+    test: function () {
+      this.isHome = !(this.isHome);
+      alert(this.isHome);
+    },
+    onClickHome: function () {
+      this.isHome = true;
+      this.isAttractions = false;
+      this.isResturants = false;
+      this.isAccomodation = false;
+      this.isTravel = false;
+      
+      this.items[0].disabled = true;
+      this.items[1].disabled = false;
+      this.items[2].disabled = false;
+      this.items[3].disabled = false;
+      this.items[4].disabled = false;
+    },
+    onClickAttractions: function () {
+      this.isHome = false;
+      this.isAttractions = true;
+      this.isResturants = false;
+      this.isAccomodation = false;
+      this.isTravel = false;
+      
+      this.items[0].disabled = false;
+      this.items[1].disabled = true;
+      this.items[2].disabled = false;
+      this.items[3].disabled = false;
+      this.items[4].disabled = false;
+    },
+    onClickResturants: function () {
+      this.isHome = false;
+      this.isAttractions = false;
+      this.isResturants = true;
+      this.isAccomodation = false;
+      this.isTravel = false;
+      
+      this.items[0].disabled = false;
+      this.items[1].disabled = false;
+      this.items[2].disabled = true;
+      this.items[3].disabled = false;
+      this.items[4].disabled = false;
+    },
+    onClickAccomodation: function () {
+      this.isHome = false;
+      this.isAttractions = false;
+      this.isResturants = false;
+      this.isAccomodation = true;
+      this.isTravel = false;
+      
+      this.items[0].disabled = false;
+      this.items[1].disabled = false;
+      this.items[2].disabled = false;
+      this.items[3].disabled = true;
+      this.items[4].disabled = false;
+    },
+    onClickTravel: function () {
+      this.isHome = false;
+      this.isAttractions = false;
+      this.isResturants = false;
+      this.isAccomodation = false;
+      this.isTravel = true;
+      
+      this.items[0].disabled = false;
+      this.items[1].disabled = false;
+      this.items[2].disabled = false;
+      this.items[3].disabled = false;
+      this.items[4].disabled = true;
+    }
   },
   name: 'App'
 };
